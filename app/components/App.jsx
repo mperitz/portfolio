7eb54695395e2f565eb8440e14404ac0bd06c8cf
Mike Peritz <mike.peritz@gmail.com>
2017-06-12 10:04:14 -0400

    changed intro fonts

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -5,9 +5,9 @@ const App = ({ children }) => {
     <div>
       <div className="top-bar">
         <div className="center">
-          <div id="mike"><h3>Mike</h3></div>
+          <div id="mike"><h3 className="top-bar-text">Mike</h3></div>
           <div id="face"><img src="./img/mike-logo.png" /></div>
-          <div id="peritz"><h3>Peritz</h3></div>
+          <div id="peritz"><h3 className="top-bar-text">Peritz</h3></div>
         </div>
       </div>
       <div>{ children }</div>
