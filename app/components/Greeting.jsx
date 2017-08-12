0fc0b01175f903fd7c7f3f507bee64fa286fc353
Mike Peritz <mike.peritz@gmail.com>
2017-08-12 15:40:11 -0400

    changed greeting

--- app/components/Greeting.jsx
+++ app/components/Greeting.jsx
@@ -4,7 +4,7 @@ export default (props) => {
   return (
     <div className="row">
       <div className="col-xs-6"><img className="face-large" src="./img/face.png" /></div>
-      <div className="col-xs-6 greeting-text-div"><p className="greeting-text">Hi, my name is Mike!  I am a full stack JavaScript developer based out of NYC.  I recently completed Fullstack Academy's 3 month immersive software engineering course, where I am currently a teaching fellow.  In my free time, I love hiking, live music, and travel.  Scroll down to learn more about me, see some of my work, and connect!</p></div>
+      <div className="col-xs-6 greeting-text-div"><p className="greeting-text">Hi, my name is Mike!  I am a full stack JavaScript developer based out of NYC.  I recently completed Fullstack Academy's immersive software engineering course, and am now working as a Javascript Engineer for an advertisement technology firm called Receptiv.  In my free time, I love hiking, live music, and travel.  Scroll down to learn more about me, see some of my work, and connect!</p></div>
     </div>
   )
 }
